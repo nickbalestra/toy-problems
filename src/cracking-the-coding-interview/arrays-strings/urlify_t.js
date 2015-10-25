91e77b6c0d45bbe20480778c2d3b6bc9552bdbe6
Nick Balestra <nick@balestra.ch>
2015-10-25 12:11:28 -0700

    urlify

--- src/cracking-the-coding-interview/arrays-strings/urlify_t.js
+++ src/cracking-the-coding-interview/arrays-strings/urlify_t.js
@@ -6,18 +6,20 @@
 
 // test > 1.3a replace all spaces in a string with %20
 // # urlify('Mr John Smith       ', 13) == 'Mr%20John%20Smith'
-exports.urlify = urlify = function(str, len) {
-  return str.slice(0,len).split(' ').join('%20')
-}
+exports.urlify = function(str, len) {
+  return str.slice(0,len).split(' ').join('%20');
+};
 
 // test > 1.3b replace all spaces in a string with %20
 // # urlify2('Mr John Smith       ', 13) == 'Mr%20John%20Smith'
-exports.urlify2 = urlify2 = function(str, len) {
+exports.urlify2 = function(str, len) {
   var chars = [];
-  for(var i = 0; i < len; i++) {
-    if (str.charAt(i) === ' ') chars[i] = '%20';
-    else chars[i] = str.charAt(i);
+  for (var i = 0; i < len; i++) {
+    if (str.charAt(i) === ' ') {
+      chars[i] = '%20';
+    } else {
+      chars[i] = str.charAt(i);
+    }
   }
   return chars.join('');
-}
-
+};
