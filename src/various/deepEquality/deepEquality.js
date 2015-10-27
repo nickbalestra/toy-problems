9fed6d19e6042454f1b134f90be8857f2078468f
Nick Balestra <nick@balestra.ch>
2015-10-26 20:52:42 -0700

    deepequality

--- src/various/deepEquality/deepEquality.js
+++ src/various/deepEquality/deepEquality.js
@@ -11,5 +11,33 @@
   * don't worry about handling cyclical object structures.
   *
   */
+
+// test > deepEquals
+// # deepEquals({a:1, b: {c:3}},{a:1, b: {c:3}}) === true (check object deep equality)
+// # deepEquals({a:1, b: {c:5}},{a:1, b: {c:6}}) === false (check object deep equality)
 exports.deepEquals = function(apple, orange) {
+  if (apple === orange) {
+    return true;
+  }
+  if (!orange || !apple) {
+    return false;
+  }
+  if (!(apple instanceof Object) || !(orange instanceof Object)) {
+    return false;
+  }
+  var appleKeys = Object.keys(apple);
+  var orangeKeys = Object.keys(orange);
+  if (appleKeys.length !== orangeKeys.length) {
+    return false;
+  }
+  if (appleKeys.length === 0) {
+    return true;
+  }
+  for (var i = 0; i < appleKeys.length; i++) {
+    if (!exports.deepEquals(apple[appleKeys[i]], orange[appleKeys[i]])) {
+      return false;
+    }
+  }
+  return true;
 };
+
--- test/deepEquality_tapeSpec.js
+++ test/deepEquality_tapeSpec.js
@@ -1,2 +1,8 @@
 var test = require('tape');
 var file = require('../src/various/deepEquality/deepEquality.js');
+
+test('deepEquals', function(t) { 
+  t.plan(2);
+  t.deepEqual(true, file.deepEquals({a:1, b: {c:3}},{a:1, b: {c:3}}), 'check object deep equality');
+  t.deepEqual(false, file.deepEquals({a:1, b: {c:5}},{a:1, b: {c:6}}), 'check object deep equality');
+});
