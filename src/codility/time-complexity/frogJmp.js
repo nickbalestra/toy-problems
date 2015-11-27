b67348dce97e9d4de8ec9c5163dec863683af289
Nick Balestra <nick@balestra.ch>
2015-11-27 15:50:31 -0800

    added frogjump tp solution and tests

---
+++ src/codility/time-complexity/frogJmp.js
@@ -0,0 +1,23 @@
+/*
+ * frogJmp.
+ * A frog wants to get to the other side of the road.
+ * The frog located at position X and wants to get to a position greater than or equal to Y.
+ * The  frog always jumps a fixed distance, D.
+ * Count the minimal number of jumps that the frog must perform to reach its target..
+ *
+ * Requirements & constraints:
+ * https://codility.com/programmers/task/frog_jmp
+ */
+
+// test > frogJmp
+// # frogJmp(10, 85, 30) === 3 (example test)
+// # frogJmp(5, 105, 3) === 34 (two elements)
+// # frogJmp(2, 11, 3) === 3 (two elements)
+// # frogJmp(1, 1, 3) === 0 (no jump needed)
+// # frogJmp(1000000000, 1000000000, 1) === 0 (no jump needed)
+// # frogJmp(1999999999, 1000000000, 1000000000) === 0 (one big jump)
+//
+// Time complexity: O(1)
+exports.frogJmp = function(X, Y, D) {
+  return Math.ceil((Y - X) / D);
+};
