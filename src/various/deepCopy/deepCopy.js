7923d1f14e0482ef5e34b54a675ab0371bec9973
Nick Balestra <nick@balestra.ch>
2015-12-01 16:01:33 -0800

    better tests

--- src/various/deepCopy/deepCopy.js
+++ src/various/deepCopy/deepCopy.js
@@ -1,12 +1,17 @@
 /**
  * Write a function `deepCopy(obj)` which takes an object as argument and returns a
  * deep copy of the same object. Don't use JSON.stringify/JSON.parse methods.
+ * if the object contains array with holes, those should be preserved, ie: [1,,3]
  */
 
 
 // test > deepCopy
-// # deepCopy({a:1, b: {b:2, c:3}, d: null, e: [1,2,3, [3, ,5]]}) === {a:1,b:{b:2,c:3},d:null,e:[1,2,3,[3,,5]]} (retur deep copy)
+// # deepCopy({a:1, b: 2}) === {a:1,b:2} (return deep copy)
+// # deepCopy({a:1, b: {b:2, c:3}}) === {a:1,b:{b:2,c:3}} (return deep copy with nested objects)
+// # deepCopy({a:1, b: {b:2, c:3}, d: null, e: [1,2,3, [3, ,5]]}) === {a:1,b:{b:2,c:3},d:null,e:[1,2,3,[3,,5]]} (return deep copy with holes arrays)
+
 exports.deepCopy = function(obj) {
+
   var copy = Array.isArray(obj) ? [] : {};
 
   var keys = Object.keys(obj);
--- test/deepCopy_tapeSpec.js
+++ test/deepCopy_tapeSpec.js
@@ -2,6 +2,8 @@ var test = require('tape');
 var file = require('../src/various/deepCopy/deepCopy.js');
 
 test('deepCopy', function(t) { 
-  t.plan(1);
-  t.deepEqual({a:1,b:{b:2,c:3},d:null,e:[1,2,3,[3,,5]]}, file.deepCopy({a:1, b: {b:2, c:3}, d: null, e: [1,2,3, [3, ,5]]}), 'retur deep copy');
+  t.plan(3);
+  t.deepEqual({a:1,b:2}, file.deepCopy({a:1, b: 2}), 'return deep copy');
+  t.deepEqual({a:1,b:{b:2,c:3}}, file.deepCopy({a:1, b: {b:2, c:3}}), 'return deep copy with nested objects');
+  t.deepEqual({a:1,b:{b:2,c:3},d:null,e:[1,2,3,[3,,5]]}, file.deepCopy({a:1, b: {b:2, c:3}, d: null, e: [1,2,3, [3, ,5]]}), 'return deep copy with holes arrays');
 });
