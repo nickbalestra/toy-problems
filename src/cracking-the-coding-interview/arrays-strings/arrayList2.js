6656eff552c073c90daca66b9dd20f8e63a33703
Nick Balestra <nick@balestra.ch>
2015-12-21 20:20:27 -0800

    added simple arrayList using built in arrays

---
+++ src/cracking-the-coding-interview/arrays-strings/arrayList2.js
@@ -0,0 +1,45 @@
+// This class represents a dynamic array functionality.
+// It will ensure that we don't accidentally try to use up too much space.
+// It uses a limited array DS for internal storage, and offer pop, push and length methids.
+var ArrayList = function() {
+  var _size = 0;
+  var _limit = 10;
+  var _storage = new Array(_limit);
+
+
+  this.push = function(value) {
+    if (_size > _limit * .75) {
+      this.resize(_limit * 2);
+    }
+    _storage[_size++] = value;
+  };
+
+  this.pop = function() {
+    if (_size > 0 && _size < _limit * .25) {
+      this.resize(_limit / 2);
+    }
+    if (_size > 0) {
+      return _storage[--_size];
+    }
+  };
+
+  this.resize = function(newLimit) {
+    var oldStorage = _storage;
+    _storage = new Array(newLimit);
+    _limit = newLimit;
+
+    oldStorage.forEach(function(el, i) {
+      if (i < _size) {
+        _storage[i] = el;
+      }
+    });
+  };
+
+  this.length = function() {
+    console.log(_storage.length)
+    return _size;
+  };
+};
+
+
+
