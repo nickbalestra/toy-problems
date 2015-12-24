92607c2bed866dcbc245fbbd3fc5e6f927300d03
Nick Balestra <nick@balestra.ch>
2015-12-23 20:56:10 -0800

    fixed test number

--- test/rotateMatrix_tapeSpec.js
+++ test/rotateMatrix_tapeSpec.js
@@ -1,7 +1,7 @@
 var test = require('tape');
 var file = require('../src/cracking-the-coding-interview/arrays-strings/rotateMatrix.js');
 
-test('1.6 rotateMatrix', function(t) { 
+test('1.7 rotateMatrix', function(t) { 
   t.plan(6);
   t.deepEqual([[13,9,5,1],[14,10,6,2],[15,11,7,3],[16,12,8,4]], file.rotateMatrix(file.matrix), 'rotate 90 degree cw the matrix as default');
   t.deepEqual([[13,9,5,1],[14,10,6,2],[15,11,7,3],[16,12,8,4]], file.rotateMatrix(file.matrix, 90), 'rotate 90 degree cw the matrix');
@@ -11,7 +11,7 @@ test('1.6 rotateMatrix', function(t) {
   t.deepEqual([[13,9,5,1],[14,10,6,2],[15,11,7,3],[16,12,8,4]], file.rotateMatrix(file.matrix, -270), 'rotate 270 degree ccw the matrix');
 });
 
-test('1.6b rotateMatrixInPlace', function(t) { 
+test('1.7b rotateMatrixInPlace', function(t) { 
   t.plan(6);
   t.deepEqual([[13,9,5,1],[14,10,6,2],[15,11,7,3],[16,12,8,4]], file.rotateMatrixInPlace(file.matrix), 'rotate 90 degree cw the matrix as default');
   t.deepEqual([[16,15,14,13],[12,11,10,9],[8,7,6,5],[4,3,2,1]], file.rotateMatrixInPlace(file.matrix, 90), 'rotate 90 degree cw the matrix');
